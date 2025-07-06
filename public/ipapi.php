dbc02d1f8cfb96c6902a2583e5b774930d4a3dcd
erlandv <erlandramdhani@gmail.com>
2025-07-06 18:17:16 +0700

    fetch ip api from backend

--- public/ipapi.php
+++ public/ipapi.php
@@ -16,5 +16,19 @@ if (!filter_var($ip, FILTER_VALIDATE_IP)) {
 }
 
 $url = "https://freeipapi.com/api/json/$ip";
-$response = file_get_contents($url);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo json_encode(['error' => 'Curl Error: ' . curl_error($ch)]);
+    curl_close($ch);
+    exit;
+}
+
+curl_close($ch);
 echo $response;
