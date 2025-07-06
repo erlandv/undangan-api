a367b63f3c77bfc5d0934cdaf537b3117de95e35
erlandv <erlandramdhani@gmail.com>
2025-07-06 18:09:44 +0700

    fetch ip api from backend

--- public/ipapi.php
+++ public/ipapi.php
@@ -8,14 +8,13 @@ if (!isset($_GET['ip'])) {
     exit;
 }
 
+$ip = $_GET['ip'];
+
 if (!filter_var($ip, FILTER_VALIDATE_IP)) {
     echo json_encode(['error' => 'Invalid IP']);
     exit;
 }
 
-$ip = $_GET['ip'];
 $url = "https://freeipapi.com/api/json/$ip";
-
-// Fetch from API
 $response = file_get_contents($url);
 echo $response;
